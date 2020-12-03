3e2b5335f324a086cdb53cc3841c515496ad6dd4
Curtis Delicata <curtis.delicata@outlook.com>
2020-12-03 21:56:48 +0000

    Meta data.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,53 @@
+import getSiteMeta from "./utils/getSiteMeta";
+
+const meta = getSiteMeta();
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'frontend',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ],
-    script: [
-      {
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'
-      }
-    ],
+    htmlAttrs: {
+    lang: "en-GB",
   },
+  title: "Family Tree 365 - Start your family tree today - free! Your first tree is 100% free. Sign-up to begin your genealogy journey today!",
+  meta: [
+    ...meta,
+    { charset: "utf-8" },
+    { name: "HandheldFriendly", content: "True" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { property: "og:site_name", content: "Family Tree 365" },
+    {
+      hid: "description",
+      name: "description",
+      content:
+        "Our user-friendly yet powerful platform lets you create your own family tree the quick and easy way. No technical knowledge is required. Start your family tree today - free!",
+    },
+    { property: "og:image:width", content: "2500" },
+    { property: "og:image:height", content: "780" },
+    { name: "twitter:site", content: "@familytree365" },
+    { name: "twitter:card", content: "summary_large_image" },
+  ],
+  link: [
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    {
+      hid: "canonical",
+      rel: "canonical",
+      href: process.env.BASE_URL,
+    },
+
+  ],
+  script: [
+    {
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'
+    }
+  ],
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-  ],
+  ]
+    },
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
