ea5d27c2609e72af3aeba9ed1c9ffd7300908a84
Curtis Delicata <curtis.delicata@outlook.com>
2021-08-17 21:04:20 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,8 +7,8 @@ export default {
   ssr: false,
   
     server: {
-    host: '0', // default: localhost
-    port: '443'
+    host: process.env.HOST || '0', // default: localhost
+    port: '80'
   },
 
   publicRuntimeConfig: {
